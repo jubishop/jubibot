51e7df91f87b15777f0c8df87fdaca9695148a21
Justin Bishop <jubishop@gmail.com>
2021-02-18 21:58:44 -0800

    csv parse by mobile chars

--- lib/jubibot.rb
+++ lib/jubibot.rb
@@ -212,8 +212,12 @@ class JubiBot
     message = shift_command(event)
     return unless message
 
+    message.strip!
     begin
-      params = CSV.parse_line(message.strip, col_sep: ' ').compact
+      quote_char = message.include?('“') ? '“' : '"'
+      params = CSV.parse_line(message,
+                              col_sep: ' ',
+                              quote_char: quote_char).compact
     rescue CSV::MalformedCSVError
       return 'It appears you used an unclosed " in your command.'
     end
