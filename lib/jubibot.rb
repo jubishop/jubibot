2e2e4912f1ac403f1653d4dc2f8eac5430c19f3b
Justin Bishop <jubishop@gmail.com>
2021-02-20 21:52:17 -0800

    safer management of commands becoming nil

--- lib/jubibot.rb
+++ lib/jubibot.rb
@@ -210,15 +210,17 @@ class JubiBot
 
   def process_command(event)
     message = shift_command(event)
-    return unless message
+    return if message.nil? || message.empty?
 
     begin
       message.strip!
       message.gsub!(/“|”/, '"')
-      params = CSV.parse_line(message, col_sep: ' ').compact
+      params = CSV.parse_line(message, col_sep: ' ')&.compact
     rescue CSV::MalformedCSVError
       return 'It appears you used an unclosed " in your command.'
     end
+
+    return unless params
 
     command_name = command_name(params.shift)
     return help_message(event, params) if command_name == :help
