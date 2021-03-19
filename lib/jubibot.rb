dc3cbbc5c122afdb67e424310d0cb6e6e878b2f1
Justin Bishop <jubishop@gmail.com>
2021-03-19 08:08:34 -0700

    smarter help

--- lib/jubibot.rb
+++ lib/jubibot.rb
@@ -245,7 +245,7 @@ class JubiBot
       qualifier = num_args > command.num_args.max ? 'Too many' : 'Not enough'
       response = "#{qualifier} args passed to `#{command_name}`."
       if @docs.key?(command_name)
-        return help_message(event, [command_name])
+        return help_message(event, [command_name] + params)
         # response << "  Try `#{cmd(event, 'help')} #{command_name}`."
       end
       return response
