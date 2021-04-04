27c387bdc2409bd034ac8eae069d8d7d57a15b94
Justin Bishop <jubishop@gmail.com>
2021-04-04 13:00:10 -0700

    better spec syntax

--- spec/jubibot/command_spec.rb
+++ spec/jubibot/command_spec.rb
@@ -4,50 +4,45 @@ require_relative '../lib/fake_bot'
 require_relative '../lib/fake_event'
 
 RSpec.describe(JubiBot, '#command') {
-  shared_context('ping bot') {
+  shared_examples('a ping bot') { |**params|
     before {
+      @prefix = params.fetch(:prefix, '!')
+      @prefix = @prefix.call if @prefix.is_a?(Proc)
+
       @fake_bot = Discordrb::FakeBot.new
       allow(Discordrb::Bot).to(receive(:new) { @fake_bot })
-
       @command_bot = double(Object, ping: nil)
-    }
-
-    def create_jubibot(**params)
       jubi_bot = JubiBot.new(token: 'tkn', command_bot: @command_bot, **params)
       jubi_bot.command('ping')
       jubi_bot.run
-    end
-  }
-
-  context('processing basic commands') {
-    include_context('ping bot')
-    before { create_jubibot }
+    }
 
     it('passes along basic ping command') {
-      @fake_bot.incoming_message(FakeEvent.new('!ping'))
+      @fake_bot.incoming_message(FakeEvent.new("#{@prefix}ping"))
       expect(@command_bot).to(have_received(:ping))
     }
 
     it('ignores commands that are not registered') {
-      @fake_bot.incoming_message(FakeEvent.new('!pong'))
+      @fake_bot.incoming_message(FakeEvent.new("#{@prefix}pong"))
     }
 
-    it('ignores messages that aren not commands') {
+    it('ignores messages that are not commands') {
       @fake_bot.incoming_message(FakeEvent.new('ping'))
       expect(@command_bot).not_to(have_received(:ping))
     }
   }
 
+  context('processing basic commands') {
+    it_behaves_like('a ping bot')
+  }
+
   context('changing the server prefix') {
-    include_context('ping bot')
+    context('with a basic string') {
+      it_behaves_like('a ping bot', { prefix: '#' })
+    }
 
-    context('works with a basic string') {
-      before { create_jubibot(prefix: '#') }
-
-      it('passes along basic ping command') {
-        @fake_bot.incoming_message(FakeEvent.new('#ping'))
-        expect(@command_bot).to(have_received(:ping))
-      }
+    context('with a proc') {
+      it_behaves_like('a ping bot', { prefix: proc { '@' } })
     }
   }
 }
