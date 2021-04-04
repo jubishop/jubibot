a8952585ac2bde933f0b8751c236241e113a30ab
Justin Bishop <jubishop@gmail.com>
2021-04-04 11:36:14 -0700

    adding to rspec.

--- spec/jubibot/command_spec.rb
+++ spec/jubibot/command_spec.rb
@@ -4,17 +4,50 @@ require_relative '../lib/fake_bot'
 require_relative '../lib/fake_event'
 
 RSpec.describe(JubiBot, '#command') {
-  it('processes basic command') {
-    fake_bot = Discordrb::FakeBot.new
-    allow(Discordrb::Bot).to(receive(:new) { fake_bot })
+  shared_context('ping bot') {
+    before {
+      @fake_bot = Discordrb::FakeBot.new
+      allow(Discordrb::Bot).to(receive(:new) { @fake_bot })
 
-    command_bot = double(Object, ping: nil)
-    jubi_bot = JubiBot.new(token: 'one', command_bot: command_bot)
+      @command_bot = double(Object, ping: nil)
+    }
 
-    jubi_bot.command('ping')
-    jubi_bot.run
+    def create_jubibot(**params)
+      jubi_bot = JubiBot.new(token: 'tkn', command_bot: @command_bot, **params)
+      jubi_bot.command('ping')
+      jubi_bot.run
+    end
+  }
 
-    fake_bot.incoming_message(FakeEvent.new('!ping'))
-    expect(command_bot).to(have_received(:ping))
+  context('processing basic commands') {
+    include_context('ping bot')
+    before { create_jubibot }
+
+    it('passes along basic ping command') {
+      @fake_bot.incoming_message(FakeEvent.new('!ping'))
+      expect(@command_bot).to(have_received(:ping))
+    }
+
+    it('ignores commands that are not registered') {
+      @fake_bot.incoming_message(FakeEvent.new('!pong'))
+    }
+
+    it('ignores messages that aren not commands') {
+      @fake_bot.incoming_message(FakeEvent.new('ping'))
+      expect(@command_bot).not_to(have_received(:ping))
+    }
+  }
+
+  context('changing the server prefix') {
+    include_context('ping bot')
+
+    context('works with a basic string') {
+      before { create_jubibot(prefix: '#') }
+
+      it('passes along basic ping command') {
+        @fake_bot.incoming_message(FakeEvent.new('#ping'))
+        expect(@command_bot).to(have_received(:ping))
+      }
+    }
   }
 }
--- spec/lib/fake_bot.rb
+++ spec/lib/fake_bot.rb
@@ -3,22 +3,26 @@ require 'jubibot'
 module Discordrb
   class FakeBot
     include RSpec::Matchers
-    # Stubs incoming from JubiBot
-    def initialize(**_)
-      @ran = false
-    end
+    include RSpec::Mocks::ExampleMethods
 
     attr_reader :reaction_add_block,
                 :reaction_remove_block,
                 :message_block,
                 :async,
-                :ran
+                :ran,
+                :profile
+
+    def initialize(**_)
+      @ran = false
+      @profile = double('profile').as_null_object
+    end
 
     # Helpers for RSpec
     def incoming_message(event)
       message_block.call(event)
     end
 
+    # Stubs incoming from JubiBot
     def reaction_add(&block)
       @reaction_add_block = block
     end
